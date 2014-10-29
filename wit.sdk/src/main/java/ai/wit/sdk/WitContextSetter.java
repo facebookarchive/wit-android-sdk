0f5c8893ec165c7b921c584447e07b1093beb53f
Aric Lasry <lasry.aric@gmail.com>
2014-10-29 13:58:01 -0700

    Add: auto generated reftime and gps location context

---
+++ wit.sdk/src/main/java/ai/wit/sdk/WitContextSetter.java
@@ -0,0 +1,64 @@
+package ai.wit.sdk;
+
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.util.Log;
+
+import com.google.gson.JsonObject;
+
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import android.content.Context;
+
+/**
+ * Created by aric on 10/29/14.
+ */
+public class WitContextSetter {
+
+    private static String KEY_REFTIME = "reference_time";
+    private static String KEY_LOCATION = "location";
+    private Context _androidContext;
+
+    public WitContextSetter(JsonObject contextData, Context androidContext) {
+        _androidContext = androidContext;
+        ensureTime(contextData);
+        ensureLocation(contextData);
+    }
+
+    protected void ensureTime(JsonObject context) {
+        if (context.get(KEY_REFTIME) != null) {
+            return ;
+        }
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        String nowAsISO8601 = df.format(new Date());
+        context.addProperty(KEY_REFTIME, nowAsISO8601);
+    }
+
+    protected void ensureLocation(JsonObject context) {
+        if (context.get(KEY_LOCATION) != null || _androidContext == null) {
+            return ;
+        }
+        Location loc = getLocation();
+        JsonObject jsonLocation = new JsonObject();
+        jsonLocation.addProperty("latitude", loc.getLatitude());
+        jsonLocation.addProperty("longitude", loc.getLongitude());
+        context.add(KEY_LOCATION, jsonLocation);
+    }
+
+    /**
+     * This function should only be called when the _androidContext instance variable is set
+     * @return Location object
+     */
+    protected Location getLocation() {
+        LocationManager lm = (LocationManager) _androidContext.getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);
+        String locationProvider = lm.getBestProvider(criteria, true);
+        Location loc = lm.getLastKnownLocation(locationProvider);
+
+        return loc;
+    }
+}
