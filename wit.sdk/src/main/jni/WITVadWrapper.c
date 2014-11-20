b5901e6bf3ffdf651cb39a9fa40848744c9aad63
Anthony Kesich <akesich+github@gmail.com>
2014-11-19 17:40:19 -0800

    Modified wrapper to take fft components.

--- wit.sdk/src/main/jni/WITVadWrapper.c
+++ wit.sdk/src/main/jni/WITVadWrapper.c
@@ -11,28 +11,37 @@ int Java_ai_wit_sdk_WitMic_VadInit()
     int sample_rate = 16000;
     int speech_timeout = 8000; //timeout in ms
 
-    wit_vad_g_struct = wv_detector_cvad_init(sample_rate, 0, speech_timeout);
+    wit_vad_g_struct = wv_detector_cvad_init(sample_rate, 1, speech_timeout);
 
     return 0;
 }
 
 
-int Java_ai_wit_sdk_WitMic_VadStillTalking(JNIEnv *env, jobject obj, jshortArray java_arr, jint arr_len)
+int Java_ai_wit_sdk_WitMic_VadStillTalking(JNIEnv *env, jobject obj, jshortArray java_arr, jfloatArray java_fft_arr)
 {
   short int *samples;
+  float *fft_mags;
   int i, sum = 0;
   int result;
   jshort *native_arr = (*env)->GetShortArrayElements(env, java_arr, NULL);
+  jfloat *native_fft_arr = (*env)->GetFloatArrayElements(env, java_fft_arr, NULL);
+  int arr_len = wit_vad_g_struct->samples_per_frame;
 
   samples = malloc(sizeof(*samples) * arr_len);
   for (i = 0; i < arr_len; i++) {
     samples[i] = native_arr[i];
-
   }
   (*env)->ReleaseShortArrayElements(env, java_arr, native_arr, 0);
 
-  result = wvs_cvad_detect_talking(wit_vad_g_struct, samples, arr_len);
+  fft_mags = malloc(sizeof(*fft_mags) * arr_len);
+  for (i = 0; i < arr_len/2; i++) {
+    fft_mags[i] = native_fft_arr[i];
+  }
+  (*env)->ReleaseFloatArrayElements(env, java_fft_arr, native_fft_arr, 0);
+
+  result = wvs_cvad_detect_talking(wit_vad_g_struct, samples, fft_mags);
   free(samples);
+  free(fft_mags);
 
   return result;
 }
@@ -43,4 +52,8 @@ void Java_ai_wit_sdk_WitMic_VadClean()
         wv_detector_cvad_clean(wit_vad_g_struct);
         wit_vad_g_struct = 0;
     }
+}
+
+int Java_ai_wit_sdk_WitMic_GetVadSamplesPerFrame(){
+    return wit_vad_g_struct->samples_per_frame;
 }
